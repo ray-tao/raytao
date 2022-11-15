a8a375ae3099da5eb5ad91fd866de8f0f7b5e371
ray-tao <taor740@newschool.edu>
2022-11-15 18:18:34 -0500

    Updated raymenu and added second Museum

---
+++ projects/museum2/week4/week4.js
@@ -0,0 +1,83 @@
+$("#line1").on("click", function () {
+    $("#line1").addClass("div-animate");
+    $("#wings1").addClass("img-animate");
+    $("#line1>h3").addClass("img-animate");
+    $("#line1").css("cursor", "default");
+
+    setTimeout(function () {
+        $("#line1").removeClass("div-animate");
+        $("#wings1").removeClass("img-animate");
+        $("#line1>h3").removeClass("img-animate");
+        $("#line1").css("cursor", "pointer");
+    }, 15000);
+});
+
+$("#line2").on("click", function () {
+    $("#line2").addClass("div-animate");
+    $("#wings2").addClass("img-animate");
+    $("#line2>h3").addClass("img-animate");
+    $("#line2").css("cursor", "default");
+
+    setTimeout(function () {
+        $("#line2").removeClass("div-animate");
+        $("#wings2").removeClass("img-animate");
+        $("#line2>h3").removeClass("img-animate");
+        $("#line2").css("cursor", "pointer");
+    }, 15000);
+});
+
+$("#line3").on("click", function () {
+    $("#line3").addClass("div-animate");
+    $("#wings3").addClass("img-animate");
+    $("#line3>h3").addClass("img-animate");
+    $("#line3").css("cursor", "default");
+
+    setTimeout(function () {
+        $("#line3").removeClass("div-animate");
+        $("#wings3").removeClass("img-animate");
+        $("#line3>h3").removeClass("img-animate");
+        $("#line3").css("cursor", "pointer");
+    }, 15000);
+});
+
+$("#line4").on("click", function () {
+    $("#line4").addClass("div-animate");
+    $("#wings4").addClass("img-animate");
+    $("#line4>h3").addClass("img-animate");
+    $("#line4").css("cursor", "default");
+
+    setTimeout(function () {
+        $("#line4").removeClass("div-animate");
+        $("#wings4").removeClass("img-animate");
+        $("#line4>h3").removeClass("img-animate");
+        $("#line4").css("cursor", "pointer");
+    }, 15000);
+});
+
+$("#line5").on("click", function () {
+    $("#line5").addClass("div-animate");
+    $("#wings5").addClass("img-animate");
+    $("#line5>h3").addClass("img-animate");
+    $("#line5").css("cursor", "default");
+
+    setTimeout(function () {
+        $("#line5").removeClass("div-animate");
+        $("#wings5").removeClass("img-animate");
+        $("#line5>h3").removeClass("img-animate");
+        $("#line5").css("cursor", "pointer");
+    }, 15000);
+});
+
+$("#line6").on("click", function () {
+    $("#line6").addClass("div-animate");
+    $("#wings6").addClass("img-animate");
+    $("#line6>h3").addClass("img-animate");
+    $("#line6").css("cursor", "default");
+
+    setTimeout(function () {
+        $("#line6").removeClass("div-animate");
+        $("#wings6").removeClass("img-animate");
+        $("#line6>h3").removeClass("img-animate");
+        $("#line6").css("cursor", "pointer");
+    }, 15000);
+});
